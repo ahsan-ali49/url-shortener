c139d9317ba1b11217b7cd9061d4fffec60dc316
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-02 22:41:40 +0500

    added model

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+require("dotenv").config()
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: false}));
 
 app.get("/", (req, res)=>{
     res.render("index");
+})
+
+app.post("/shortUrls", async (req, res)=>{
+    await ShortUrl.create({full: req.body.fullUrl})
+    res.redirect("/")
 })
 
 app.listen(process.env.PORT || 5000);
